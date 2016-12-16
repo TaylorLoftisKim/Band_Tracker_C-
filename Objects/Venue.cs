e8a338148b09990e136bda48b64e5116faddc55c
Taylor Loftis-Kim <taylorloftiskim@gmail.com>
2016-12-16 13:14:43 -0800

    Make GetAll and Save Method for Venue

--- Objects/Venue.cs
+++ Objects/Venue.cs
@@ -36,5 +36,42 @@ namespace Tracker
     {
       return _name;
     }
+
+    public static List<Venue> GetAll()
+    {
+      List<Venue> allVenue = new List<Venue>{};
+
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("SELECT * FROM venues;", conn);
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      while(rdr.Read())
+      {
+        int venueId = rdr.GetInt32(0);
+        string venueName = rdr.GetString(1);
+
+        Venue newVenue = new Venue(venueName, venueId);
+        allVenues.Add(newVenue);
+      }
+      if(rdr.Close != null)
+      {
+        rdr.Close();
+      }
+      if(conn != null)
+      {
+        conn.Close();
+      }
+      return allVenues;
+    }
+
+    public void Save()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("INSERT INTO venues (name) OUTPUT INSERTED.id VALUES (@VenueName)")
+    }
   }
 }
